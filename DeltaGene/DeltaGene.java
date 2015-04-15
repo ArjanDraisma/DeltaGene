88108d1139cdf07b745531c6d44563c9afb84210
CrudeOil <adraisma@gmail.com>
2015-04-15 14:27:34 +0200

    Updated DeltaGene.jar

--- DeltaGene/DeltaGene.java
+++ DeltaGene/DeltaGene.java
@@ -37,8 +37,6 @@
 
 package DeltaGene;
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
